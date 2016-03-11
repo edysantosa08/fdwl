c6b059d98e730c3589ad809764125bf031b646e6
unknown <think.actions@gmail.com>
2016-03-12 04:07:59 +0700

    coba coba soundclick native

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -4,12 +4,20 @@ module.exports = [
         "file": "plugins/cordova-plugin-whitelist/whitelist.js",
         "id": "cordova-plugin-whitelist.whitelist",
         "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-nativeclicksound/www/nativeclick.js",
+        "id": "cordova-plugin-nativeclicksound.nativeclick",
+        "clobbers": [
+            "nativeclick"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.2.1"
+    "cordova-plugin-whitelist": "1.2.1",
+    "cordova-plugin-nativeclicksound": "0.0.3"
 };
 // BOTTOM OF METADATA
 });
